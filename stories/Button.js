0ba05433ccc64cc6f2aa91dcbdd15503c1eb971d
James <james@jarofgreen.co.uk>
2025-05-11 19:19:11 +0100

    Move button wrapper out of misc to button section and add HTML example

--- stories/Button.js
+++ stories/Button.js
@@ -4,7 +4,11 @@ import './button.css';
 export const getButtonHTML = () => {
 
   const html = `
-         <a href="https://www.datatig.com/" class="button">More information</a>
+    <div class="links">
+      <a href="https://www.datatig.com/" class="button">More information 1</a>
+      <a href="https://www.datatig.com/" class="button">More information 2</a>
+      <a href="https://www.datatig.com/" class="button">More information 3</a>
+    </div>
   `;
 
   return html;
